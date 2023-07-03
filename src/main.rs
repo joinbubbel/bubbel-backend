6d8268fc7fb19ea2a729f99ee41c1bd9e61ebb36
dav <usr21p1@gmail.com>
2023-07-02 17:47:01 -0700

    Deployment try #2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
