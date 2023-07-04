b7884a1f9689dd237d0c19b9fc1413dd1ca6b98b
dav <usr21p1@gmail.com>
2023-07-04 08:10:54 -0700

    Remove database test early return.

--- bubbel-bath/src/tests/mod.rs
+++ bubbel-bath/src/tests/mod.rs
@@ -1,16 +1,13 @@
 use super::{schema::users::dsl::*, *};
 use diesel::{pg::PgConnection, prelude::*};
 
-pub fn new_test_database() -> Option<PgConnection> {
+pub fn new_test_database() -> PgConnection {
     let pg = "postgresql://postgres:abc@localhost:5432";
-    PgConnection::establish(pg).ok()
+    PgConnection::establish(pg).unwrap()
 }
 
 #[test]
 pub fn test() {
-    let Some(mut conn) = new_test_database() else {
-        eprintln!("Early disconnect, test database not running.");
-        return;
-    };
+    let mut conn = new_test_database();
     let _ = users.select(User::as_select()).load(&mut conn).unwrap();
 }
