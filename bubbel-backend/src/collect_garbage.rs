35e74ea05a0d424e1d49fa60a29a66cc65dcb9ff
dav <usr21p1@gmail.com>
2023-07-15 09:02:37 -0700

    Change garbage collection times (again).

--- bubbel-backend/src/collect_garbage.rs
+++ bubbel-backend/src/collect_garbage.rs
@@ -11,11 +11,11 @@ pub async fn collect_garbage(app: Arc<AppState>) {
 const AUTH_COLLECT_GARBAGE_INTERVAL: Duration = Duration::from_secs(43200);
 async fn collect_auth_garbage(app: &Arc<AppState>) {
     loop {
+        tokio::time::sleep(AUTH_COLLECT_GARBAGE_INTERVAL).await;
         {
             let mut auth = app.auth.write().unwrap();
             auth.collect_garbage();
         }
-        tokio::time::sleep(AUTH_COLLECT_GARBAGE_INTERVAL).await;
     }
 }
 
@@ -23,11 +23,11 @@ async fn collect_auth_garbage(app: &Arc<AppState>) {
 const ACC_LIMBO_COLLECT_GARBAGE_INTERVAL: Duration = Duration::from_secs(43200);
 async fn collect_acc_limbo_garbage(app: &Arc<AppState>) {
     loop {
+        tokio::time::sleep(AUTH_COLLECT_GARBAGE_INTERVAL).await;
         {
             let mut db = app.db.lock().unwrap();
             let mut acc_limbo = app.acc_limbo.lock().unwrap();
             acc_limbo.collect_garbage(&mut db);
         }
-        tokio::time::sleep(ACC_LIMBO_COLLECT_GARBAGE_INTERVAL).await;
     }
 }
