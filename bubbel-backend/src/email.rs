61d88eccda1aa183bfdf97c1ccc60b10a132c471
Ivoine <ivoinestrachan242@gmail.com>
2023-07-24 12:28:02 -0400

    fixed html styling

--- bubbel-backend/src/email.rs
+++ bubbel-backend/src/email.rs
@@ -23,7 +23,7 @@ pub fn send_verify_account_email(
         .body(format!( r#"
         <html>
         <body style="font-family: Arial, sans-serif;">
-        <img src="{}" alt="Bubbel Logo" style="max-width: 16px;" />
+        <img src="{}" alt="Bubbel Logo" style="max-width: 24px;" />
             <p>Hi 👋,</p>
             <p>You requested a verification code to sign up for Bubbel. It's here:</p>
             <p style="font-size: 18px;">
