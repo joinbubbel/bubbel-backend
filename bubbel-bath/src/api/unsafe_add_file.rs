b04111c6670c30a914231cffaeacd5167e1bab60
Ivoine <ivoinestrachan242@gmail.com>
2023-08-27 17:12:45 -0400

    putting stuff back to normal

--- bubbel-bath/src/api/unsafe_add_file.rs
+++ bubbel-bath/src/api/unsafe_add_file.rs
@@ -1,4 +1,3 @@
-use base64::{Engine, engine::general_purpose};
 use super::*;
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq, Eq)]
@@ -20,25 +19,10 @@ pub enum UnsafeAddFileError {
 
 pub fn unsafe_add_file(req: UnsafeAddFile) -> Result<UnsafeAddFileOut, UnsafeAddFileError> {
     let token = generate_token_alphanumeric(32);
-    let engine = general_purpose::STANDARD_NO_PAD;
-
-   
-    let clean_data = req.data.replace(" ", "").replace("\n", "").replace("\r", "");
-    let padded_data = if clean_data.len() % 4 != 0 {
-        clean_data.clone() + &"=".repeat(4 - (clean_data.len() % 4))
-    } else {
-        clean_data
-    };
-
-   
-    let decoded_data = engine.decode(padded_data.as_bytes())
-        .map_err(|e| UnsafeAddFileError::Internal { ierror: e.to_string() })?;
-
     let write_dir = "/tmp/unsafe_data/".to_owned() + &token + &req.extension;
-
-    std::fs::write(write_dir, &decoded_data)
-        .map_err(|e| UnsafeAddFileError::Internal { ierror: e.to_string() })?;
-
+    std::fs::write(write_dir, req.data.as_bytes()).map_err(|e| UnsafeAddFileError::Internal {
+        ierror: e.to_string(),
+    })?;
     Ok(UnsafeAddFileOut {
         file_link: format!(
             "https://api.joinbubbel.com/unsafe_data/{}{}",
