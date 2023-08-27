930f78d54be10ab49abef04c990f5f79d2fb152a
Ivoine <ivoinestrachan242@gmail.com>
2023-08-27 16:29:25 -0400

    fixed invalid padding

--- bubbel-bath/src/api/unsafe_add_file.rs
+++ bubbel-bath/src/api/unsafe_add_file.rs
@@ -20,14 +20,20 @@ pub enum UnsafeAddFileError {
 
 pub fn unsafe_add_file(req: UnsafeAddFile) -> Result<UnsafeAddFileOut, UnsafeAddFileError> {
     let token = generate_token_alphanumeric(32);
-
     let engine = general_purpose::STANDARD_NO_PAD;
 
-    let decoded_data = engine.decode(req.data.as_bytes())
+    let clean_data = req.data.replace(" ", "").replace("\n", "").replace("\r", "");
+    let padded_data = if clean_data.len() % 4 != 0 {
+        clean_data + &"=".repeat(4 - (clean_data.len() % 4))
+    } else {
+        clean_data
+    };
+
+  
+    let decoded_data = engine.decode(padded_data.as_bytes())
         .map_err(|e| UnsafeAddFileError::Internal { ierror: e.to_string() })?;
 
     let write_dir = "/tmp/unsafe_data/".to_owned() + &token + &req.extension;
-
 
     std::fs::write(write_dir, &decoded_data)
         .map_err(|e| UnsafeAddFileError::Internal { ierror: e.to_string() })?;
