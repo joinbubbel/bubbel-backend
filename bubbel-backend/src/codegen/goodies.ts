cc950b11a95eb440f7f4f99f2e827b63218cdf63
dav <usr21p1@gmail.com>
2023-08-30 19:32:57 -0700

    Update Goodies

--- bubbel-backend/src/codegen/goodies.ts
+++ bubbel-backend/src/codegen/goodies.ts
@@ -1,7 +1,7 @@
-function bubbelDumpsterProfilePicture150x150(objectName: string): string {
+export function bubbelDumpsterProfilePicture150x150(objectName: string): string {
     return `https://api.joinbubbel.com/dumpster/profile_picture/${objectName}/pfp150x150.jpeg`;
 }
 
-function bubbelDumpsterBannerPicture1200x200(objectName: string): string {
+export function bubbelDumpsterBannerPicture1200x200(objectName: string): string {
     return `https://api.joinbubbel.com/dumpster/banner_picturprofile_picturee/${objectName}/banner1200x200.jpeg`;
 }
